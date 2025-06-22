7b5976c88f714183910b59ea998c4171dbc2bb6e
ggarciar88 <ggarciar88@gmail.com>
2025-06-23 00:13:40 +0200

    Create clientTcp.c

---
+++ tcp/clientTcp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *message = "Hola desde el cliente TCP";
+    char buffer[1024] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    send(sock, message, strlen(message), 0);
+    read(sock, buffer, 1024);
+    printf("Mensaje del servidor: %s\n", buffer);
+    close(sock);
+    return 0;
+}
